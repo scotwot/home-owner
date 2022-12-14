10db4d5e502aa37c3b299311e775c9327c2b816f
scot <9129483+scotwot@users.noreply.github.com>
2022-12-14 22:23:11 +1100

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	// define port `r.Run(":8080")`
+	router.Run()
+}
